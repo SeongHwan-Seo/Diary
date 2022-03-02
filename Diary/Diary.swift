9bf5f1abae86e5bc39897bbec1b1bb36c1ce8be1
seosh5704 <seosh5704@gmail.com>
2022-03-02 21:23:05 +0900

    20220302 일기등록 기능 구현

---
+++ Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by seosh on 3/2/22.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
--- Diary/WriteDiaryViewController.swift
+++ Diary/WriteDiaryViewController.swift
@@ -7,10 +7,88 @@
 
 import UIKit
 
+protocol WriteDiaryViewDelegate: AnyObject {
+    func didSelectRegister(diary: Diary)
+}
+
 class WriteDiaryViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var confirmButton: UIBarButtonItem!
+    
+    private let datePicker = UIDatePicker()
+    private var diaryDate: Date?
+    weak var delegagate: WriteDiaryViewDelegate?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureContentsTextView()
+        self.configureDatePicker()
+        self.configureInputField()
+        self.confirmButton.isEnabled = false
     }
     
+    private func configureContentsTextView() {
+        let borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/225, alpha: 1.0) //alpha값은 0.0 ~ 1.0 의 값, 0.0과 가까울수록 투명해짐 rgb는 설정할려는 값의 220을 나누어 줘야함
+        self.contentsTextView.layer.borderColor = borderColor.cgColor //layer는 chColor로 설정 해줘야됨
+        self.contentsTextView.layer.borderWidth = 0.5
+        self.contentsTextView.layer.cornerRadius = 5.0
+    }
+    
+    private func configureDatePicker() {
+        self.datePicker.datePickerMode = .date //날짜만 선택하게
+        self.datePicker.preferredDatePickerStyle = .wheels
+        self.datePicker.addTarget(self, action: #selector(datePickerValueDidChange(_:)), for: .valueChanged)
+        self.datePicker.locale = Locale(identifier: "ko-KR")
+        self.dateTextField.inputView = self.datePicker
+    }
+    
+    private func configureInputField() {
+        self.contentsTextView.delegate = self
+        self.titleTextField.addTarget(self, action: #selector(titleTextFieldDidChange(_:)), for: .editingChanged)
+        self.dateTextField.addTarget(self, action: #selector(dateTextFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    @objc private func dateTextFieldDidChange(_ textField: UITextField) {
+        self.validateInputField()
+    }
+    
+    @objc private func titleTextFieldDidChange(_ textField: UITextField) {
+        self.validateInputField()
+    }
+    
+    @objc private func datePickerValueDidChange(_ datePicker: UIDatePicker) {
+        let formmater = DateFormatter()
+        formmater.dateFormat = "yyyy년 MM월 dd일(EEEEE)"
+        formmater.locale = Locale(identifier: "ko-KR")
+        self.diaryDate = datePicker.date
+        self.dateTextField.text = formmater.string(from: datePicker.date)
+        self.dateTextField.sendActions(for: .editingChanged)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true) //빈 화면을 눌러줄 때 키보드가 닫힘.
+    }
+    
+    @IBAction func confirmButton(_ sender: UIBarButtonItem) {
+        guard let title = self.titleTextField.text else { return }
+        guard let contents = self.contentsTextView.text else { return }
+        guard let date = self.diaryDate else { return }
+        let diary = Diary(title: title, contents: contents, date: date, isStar: false)
+        self.delegagate?.didSelectRegister(diary: diary)
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    private func validateInputField() {
+        self.confirmButton.isEnabled = !(self.titleTextField.text?.isEmpty ?? true) && !(self.dateTextField.text?.isEmpty ?? true) && !self.contentsTextView.text.isEmpty
+    }
+}
+
+extension WriteDiaryViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        self.validateInputField()
+    } //텍스트 뷰에 텍스트가 입력될 때 마다 호출 되는 메소드
 }
